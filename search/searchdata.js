50f0a54202fb82ce4da389f93a006a932af5de99
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-28 10:43:47 +0000

    Deploy ilia64/otus-print_ip to github.com/ilia64/otus-print_ip.git:gh-pages

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "ceips",
-  1: "ci",
-  2: "ceps"
+  0: "ceioprs",
+  1: "cir",
+  2: "ceps",
+  3: "o"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
