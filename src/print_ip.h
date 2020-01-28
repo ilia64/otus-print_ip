b985664e2eb02fb7f6f7b3d968ae6615e67937ae
ilia64 <github.ilia1964>
2020-01-29 01:16:36 +0300

    add is_integral and is_string WIP

--- src/print_ip.h
+++ src/print_ip.h
@@ -1,13 +1,24 @@
 #pragma ones
-
 #include "common.h"
-
 #include "type_traits.h"
+
+template<typename T>
+std::enable_if_t<std::is_integral_v<T>, std::string>
+to_string(const T& ip)
+{
+    return "is_integral";
+}
+
+template<typename T>
+std::enable_if_t<is_string_v<T>, std::string>
+to_string(const T& ip)
+{
+    return ip;
+}
 
 template<typename T>
 void print_ip(const T& ip)
 {
-    std::string result{"255.255.255.0"};
-
+    std::string result = to_string(ip);
     std::cout << result << '\n';
 }
--- src/type_traits.h
+++ src/type_traits.h
@@ -1,3 +1,23 @@
 #pragma ones
 
 #include <type_traits>
+
+
+//      is_string
+
+template <typename T>
+using string_type = std::basic_string<T, std::char_traits<T>, std::allocator<T>>;
+
+/*
+template <typename T>
+struct is_string : std::is_same<string_type<T>, std::remove_const_t<T>> {};
+*/
+
+template <typename T>
+struct is_string : public std::false_type {};
+
+template <typename T>
+struct is_string<string_type<T>> : public std::true_type {};
+
+template<typename T>
+constexpr bool is_string_v = is_string<T>::value;
